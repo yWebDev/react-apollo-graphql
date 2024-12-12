1077286329b83206c20f4427a3827d2b38ea9063
Yurii Shpakovych <yurii.shpakovych@davita.com>
2024-12-12 18:51:13 +0200

    Add Sequelize config

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import { $db } from '../../config'
+import { IModels } from '../types'
+
+// DB Connection
+const { dialect, port, host, database, username, password } = $db;
+// Connecting to the database
+const uri = `${dialect}://${username}:${password}@${host}:${port}/${database}`;
+const sequelize = new Sequelize(uri);
+// Models
+const models: IModels = {
+    User: require('./User').default(sequelize, Sequelize),
+    sequelize
+}
+export default models;
